4c8f1abe1095fcf81f17e6c08d03d887a66b9c9b
JonyPylon <jony@pylondata.com>
2023-09-15 07:25:25 +0200

    them toogle setup, need variables

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ projects.addEventListener('click', () => {
 experience.addEventListener('click', () => {
     scrollToElement('.header', 2);
 });
+
+// Theme toogle mode
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("change", () => {
+  const body = document.body;
+  if (themeToggle.checked) {
+    body.style.setProperty("--background-color", "var(--dark-background-color)");
+  } else {
+    body.style.setProperty("--background-color", "var(--light-background-color)");
+  }
+});
