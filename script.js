806ec30df41ac807d99083f1057b18e8830c442b
JonyPylon <jony@pylondata.com>
2023-09-15 20:00:25 +0200

    menu links color

--- script.js
+++ script.js
@@ -26,25 +26,38 @@ const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
 const logoImage = document.getElementById("logo-image");
 const footerImage = document.getElementById("footer-logo");
+const navLinks = document.querySelectorAll(".link a");
 
 themeToggle.addEventListener("change", () => {
-  if (themeToggle.checked) {
-    body.style.setProperty("--background-color", "var(--light-background-color)");
-    logoImage.src = "images/blue-logo.png";
-    footerImage.src = "images/blue-logo.png";
-  } else {
-    body.style.setProperty("--background-color", "var(--dark-background-color)");
-    logoImage.src = "images/logo.png";
-    footerImage.src = "images/logo.png";
-  }
+    if (themeToggle.checked) {
+        body.style.setProperty("--background-color", "var(--light-background-color)");
+        logoImage.src = "images/blue-logo.png";
+        footerImage.src = "images/blue-logo.png";
+        navLinks.forEach(link => {
+            link.style.color = "var(--nav-light-color)";
+        });
+    } else {
+        body.style.setProperty("--background-color", "var(--dark-background-color)");
+        logoImage.src = "images/logo.png";
+        footerImage.src = "images/logo.png";
+        navLinks.forEach(link => {
+            link.style.color = "var(--nav-dark-color)";
+        });
+    }
 });
 
 if (themeToggle.checked) {
-  body.style.setProperty("--background-color", "var(--light-background-color)");
-  logoImage.src = "images/blue-logo.png";
-  footerImage.src = "images/blue-logo.png";
+    body.style.setProperty("--background-color", "var(--light-background-color)");
+    logoImage.src = "images/blue-logo.png";
+    footerImage.src = "images/blue-logo.png";
+    navLinks.forEach(link => {
+        link.style.color = "var(--nav-light-color)";
+    });
 } else {
-  body.style.setProperty("--background-color", "var(--dark-background-color)");
-  logoImage.src = "images/logo.png";
-  footerImage.src = "images/logo.png";
+    body.style.setProperty("--background-color", "var(--dark-background-color)");
+    logoImage.src = "images/logo.png";
+    footerImage.src = "images/logo.png";
+    navLinks.forEach(link => {
+        link.style.color = "var(--nav-dark-color)";
+    });
 }
