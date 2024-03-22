44acb41faa87088c0b9b7b59598b971f5259b828
JoniSchimansky <jonischimansky@gmail.com>
2024-03-22 13:46:25 +0100

    hidden text animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+// Read more text
 function toggleText() {
     var hiddenText = document.getElementById("hidden-text");
+    var btn = document.getElementById("read-more-btn");
+
     if (hiddenText.style.display === "none") {
         hiddenText.style.display = "block";
     } else {
         hiddenText.style.display = "none";
+    }
+
+    if (hiddenText.style.maxHeight) {
+        hiddenText.style.maxHeight = null;
+        hiddenText.style.opacity = "0";
+        btn.textContent = "Read More";
+    } else {
+        hiddenText.style.maxHeight = hiddenText.scrollHeight + "px";
+        hiddenText.style.opacity = "1";
+        btn.textContent = "Close";
     }
 }
